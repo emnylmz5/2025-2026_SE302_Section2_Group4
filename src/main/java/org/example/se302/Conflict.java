aee044c48fea1d35b5f63ec2fa9cb5f850bbd9df
Emin Yılmaz <126156618+emnylmz5@users.noreply.github.com>
2025-12-13 16:55:42 +0300

    Conflict
    Class "conflict" added

--- src/main/java/org/example/se302/Conflict.java
+++ src/main/java/org/example/se302/Conflict.java
@@ -1,4 +1,40 @@
 package org.example.se302;
 
+import java.util.List;
+
 public class Conflict {
+
+    private ConflictType type;
+    private List<ExamSession> sessions;
+    private String description;
+
+    public Conflict(ConflictType type, List<ExamSession> sessions, String description) {
+        this.type = type;
+        this.sessions = sessions;
+        this.description = description;
+    }
+
+    public ConflictType getType() {
+        return type;
+    }
+
+    public void setType(ConflictType type) {
+        this.type = type;
+    }
+
+    public List<ExamSession> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<ExamSession> sessions) {
+        this.sessions = sessions;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
